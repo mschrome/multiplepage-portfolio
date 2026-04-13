71d61936fa813fbc718647f79a1553ec2b157c24
maplexiao <maplexiao@tencent.com>
2026-04-13 15:15:53 +0800

    feat(feature): add knocket

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from "next/font/google";
+import Script from 'next/script'
 import ThemeProvider from '@/components/ThemeProvider'
 import { siteConfig } from '@/config/content'
 
@@ -26,6 +27,10 @@ export default function RootLayout({
             </div>
           </div>
         </ThemeProvider>
+        <Script
+          src="https://trtc.io/knocket-sdk/sdk.js?identifier=1ad476c8e8c8126a1c&v=1776063718685"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
